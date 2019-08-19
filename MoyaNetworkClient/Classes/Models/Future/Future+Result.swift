e01811a255c151fbe5cd426dce6ce46b0db39fb8
Grishutin <grishutin@bifit.com>
2019-08-19 11:28:47 +0300

    Update to 0.2.1

--- MoyaNetworkClient/Classes/Models/Future/Future+Result.swift
+++ MoyaNetworkClient/Classes/Models/Future/Future+Result.swift
@@ -4,24 +4,28 @@ public typealias FutureResult<Value> = Future<Result<Value>>
 
 extension Future {
     public func mapResult<Success, NewSuccess>(_ transform: @escaping (Success) -> NewSuccess) -> FutureResult<NewSuccess>
-        where Response == Result<Success> {
-            return self.map { $0.map(transform) }
+        where Response == Result<Success>, Success: Codable {
+            return map { $0.map(transform) }
     }
 
 
     public func mapResultError<Success>(_ transform: @escaping (Error) -> Error) -> FutureResult<Success>
-        where Response == Result<Success> {
-            return self.map { $0.mapError(transform) }
+        where Response == Result<Success>, Success: Codable {
+            return map { $0.mapError(transform) }
     }
 
 
-    public func flatMapResult<Success, NewSuccess>(_ transform: @escaping (Success) -> FutureResult<NewSuccess>) -> FutureResult<NewSuccess>
-        where Response == Result<Success> {
-            return self.flatMap { result in
+    public func flatMapResult<Success, NewSuccess>(_ transform: @escaping (Success) -> FutureResult<NewSuccess>?) -> FutureResult<NewSuccess>
+        where Response == Result<Success>, Success: Codable, NewSuccess: Codable {
+            return flatMap { result in
                 FutureResult<NewSuccess> { callback in
                     switch result {
                     case let .success(s):
-                        transform(s).run { callback($0) }
+                        if let transform = transform(s) {
+                            transform.run { callback($0) }
+                        } else {
+                            callback(.failure(NSError()))
+                        }
                     case let .failure(error):
                         callback(.failure(error))
                     }
@@ -29,34 +33,39 @@ extension Future {
             }
     }
 
-    public func flatMapResultError<Success>(_ transform: @escaping (Error) -> FutureResult<Success>) -> FutureResult<Success>
-        where Response == Result<Success> {
-            return self.flatMap { result in
+    public func flatMapResultError<Success>(_ transform: @escaping (Error) -> FutureResult<Success>?) -> FutureResult<Success>
+        where Response == Result<Success>, Success: Codable {
+            return flatMap { result in
                 FutureResult<Success> { callback in
                     switch result {
                     case let .success(s):
                         callback(.success(s))
                     case let .failure(error):
-                        transform(error).run { callback($0) }
+                        if let transform = transform(error) {
+                            transform.run { callback($0) }
+                        } else {
+                            callback(.failure(error))
+                        }
                     }
                 }
             }
     }
 
     public func observeResultSuccess<Success>(_ callback: @escaping (Success) -> Void) -> Future
-        where Response == Result<Success> {
-            return self.mapResult {
+        where Response == Result<Success>, Success: Codable {
+            return mapResult {
                 callback($0)
                 return $0
             }
     }
 
     public func observeResultError<Success>(_ callback: @escaping (Error) -> Void) -> Future
-        where Response == Result<Success> {
-            return self.mapResultError {
+        where Response == Result<Success>, Success: Codable {
+            return mapResultError {
                 callback($0)
                 return $0
             }
     }
 }
+
 
